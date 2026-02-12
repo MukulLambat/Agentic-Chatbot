f845873ecf786f4f9a88bde4571eb6be3703601f
MukulLambat <mukul04lambat@gmail.com>
2026-02-12 01:46:25 +0100

    Basic Chatbot with gradio chat interface

--- scripts/run_app.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Main entry point for the application."""
-
-# TODO: Import and run your Gradio app
-# from frontend.gradio_app import demo
-# demo.launch()
-
-print("TODO: Implement the main entry point")
--- scripts/setup_env.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-"""Environment setup helper."""
-
-# TODO: Add environment setup logic
-print("TODO: Implement environment setup")
